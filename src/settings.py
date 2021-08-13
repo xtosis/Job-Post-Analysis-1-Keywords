7c2b616c26b2cb9df0af9a40ff43278abc49542c
xtosis <33601730+xtosis@users.noreply.github.com>
2021-08-13 20:49:52 +0400

    replaced sample data: indeed

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,11 @@
 from common.paths import *
+import pandas as pd
+
+
+def PANDAS_DISPLAY_SETTINGS():
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.width', None)
+    pd.set_option('max_colwidth', 100)
 
 
 class KeywordSettings:
