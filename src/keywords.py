558b1a09d9318eb4ea041b3aca292493e0abbfdd
xtosis <33601730+xtosis@users.noreply.github.com>
2021-08-16 18:18:52 +0400

    added to keywords.Process: func loadKeywords
    fixed typo

--- src/keywords.py
+++ src/keywords.py
@@ -3,18 +3,67 @@ from common.utils import PICKLE_LOAD, PICKLE_SAVE
 from common.logging import ProcessLogger
 from settings import KeywordSettings
 from preprocessing import KeywordPreprocessing
+import pandas as pd
+import copy
+
+
+REPLACE = {
+    ' and ': ' & ',
+    ' & ': ' and ',
+}
 
 
 class Process(ProcessLogger, KeywordSettings, KeywordPreprocessing):
     def __init__(self, job_post_path, keywords_path):
-        self.loadData(keywords_path)
+        self.loadKeywords(keywords_path)
         self.preprocessing(job_post_path)  # from KeywordPreprocessing
         self.process()
         self.finish()
 
-    def loadData(self, keywords_path):
-        # self.keywords = ...
-        pass
+    def loadKeywords(self, keywords_path):
+
+        def add_variation(text, collection):
+            text = text.lower()
+            collection.add(text)
+            for to_find, to_replace in REPLACE.items():
+                collection.add(text.replace(to_find, to_replace))
+            return collection
+
+        kws = pd.read_excel(keywords_path)
+        kws.drop(columns=['count', 'lower', 'type'], inplace=True)
+
+        # making sure there are no duplicate keywords
+        assert len(kws['name'].unique()) == len(kws)
+        kws.set_index('name', inplace=True, drop=True)
+
+        # filtering categories
+        filtered_categories = ['cert', 'project', 'link']
+        fil = kws.query(f'category not in {filtered_categories}')
+        kws = copy.deepcopy(fil)
+        del fil
+
+        # updating variations
+        for keyword, variations in kws['variations'].items():
+            keyword_variations = set()
+            keyword_variations = add_variation(keyword, keyword_variations)
+
+            # adding variations
+            if isinstance(variations, str):
+                variations = variations.strip()
+                variations = variations.split(', ')
+                for variation in variations:
+                    keyword_variations = add_variation(variation, keyword_variations)
+
+            # sorting variations so that longer phrases are searched first
+            temp = {'text': [], 'words': []}
+            for keyword_variation in keyword_variations:
+                temp['text'].append(keyword_variation)
+                temp['words'].append(keyword_variation.count(' ') + 1)
+            temp = pd.DataFrame(temp)
+            temp.sort_values('words', ascending=False, inplace=True)
+            kws.loc[keyword, 'variations'] = temp['text'].values.tolist()
+
+        self.keywords = kws
 
     def process(self):
         # these are stages
--- src/test.py
+++ src/test.py
@@ -1,7 +1,10 @@
 from common.paths import PATH_DATA
 from keywords import Process
+from settings import PANDAS_DISPLAY_SETTINGS
 
-PATH_JOB_POSTS = PATH_DATA + '/indeed'
-PATH_KEYWORDS = PATH_DATA + '/keywords_data_science'
+PANDAS_DISPLAY_SETTINGS()
+
+PATH_JOB_POSTS = PATH_DATA + '/indeed/samples'
+PATH_KEYWORDS = PATH_DATA + '/keywords_data_science.xlsx'
 
 test = Process(PATH_JOB_POSTS, PATH_KEYWORDS)
