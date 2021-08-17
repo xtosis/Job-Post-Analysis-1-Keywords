ade464208eb6595e7bf23bcd47aa07d74e547f23
xtosis <33601730+xtosis@users.noreply.github.com>
2021-08-17 19:41:10 +0400

    added to preprocessing.KeywordPreprocessing: func loadData

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,21 @@
 from common.utils import PICKLE_LOAD
 from common.logging import SubProcessLogger
+from common.paths import PATH_DATA
+import os
 
 
 class KeywordPreprocessing(SubProcessLogger):
 
     def preprocessing(self, path):
-        self.loadData(path)
+        raw = self.loadData(path)
 
     def loadData(self, path):
-        # self.raw  = ...
-        pass
+        raw = dict()
+        for name in os.listdir(path):
+            file_path = path + f'/{name}'
+            if os.path.isfile(file_path):
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    name = file_path.replace(PATH_DATA, '')
+                    raw[name] = f.read()
+                    print('loaded', name, raw[name][:90], '...', raw[name][-90:])
+        return raw
